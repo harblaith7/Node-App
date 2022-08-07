9cdbc556444256405cff6e85f414ec39dc4519ed
David Mulholland <davidmulholland@Davids-MacBook-Pro.local>
2022-08-07 17:47:51 +0100

    trigger ci

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // comment to trigger ci
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
